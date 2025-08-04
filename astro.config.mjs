3a06298cf4f3196991051cb7ee60a78b38b07c1b
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-04 15:44:23 -0500

    fix: ediciones en vite para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import viteConfig from './vite.config.js'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [vue()],
     server: {
-        host: '0.0.0.0',
+        host: true,
         port: 80
     },
-    vite: {
-        preview: {
-            allowedHosts: [
-                'web-tx3htnl2yqa0.up-de-fra1-k8s-1.apps.run-on-seenode.com',
-                '.apps.run-on-seenode.com',
-                '*'
-            ],
-            host: '0.0.0.0',
-            port: 80,
-            strictPort: true
-        },
-        server: {
-            host: '0.0.0.0',
-            port: 80
-        }
-    }
+    vite: viteConfig
 });
--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,12 @@ console.log('[vite.config.js]: cargado');
 
 export default {
   preview: {
-    allowedHosts: [
-      'web-tx3htnl2yqa0.up-de-fra1-k8s-1.apps.run-on-seenode.com',
-      '.apps.run-on-seenode.com', // Permite cualquier subdominio de seenode
-      '*'
-    ],
-    host: '0.0.0.0', // Permite conexiones desde cualquier IP
-    port: 80,
-    strictPort: true,
-    cors: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-    }
+    allowedHosts: ['*'], // Permite todos los hosts
+    host: true,
+    port: 80
   },
   server: {
-    host: '0.0.0.0',
-    port: 80,
-    cors: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
+    host: true,
+    port: 80
   }
 };
