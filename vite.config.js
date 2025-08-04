6f83e01b4d157f2a5b51ffc7e65dccca95cf4cb2
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-04 13:44:22 -0500

    fix: permitir host visto en Seenode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-// vite.config.js
+console.log('[vite.config.js]: cargado');
+
 export default {
   preview: {
-    allowedHosts: ['*'],
+    allowedHosts: ['web-tx3htnl2yqa0.up-de-fra1-k8s-1.apps.run-on-seenode.com'],
     host: true,
     port: 80
   }
