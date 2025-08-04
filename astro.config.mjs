6fca3cf7f094c34f8f40c144eb140d81be046e40
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-04 13:17:07 -0500

    Ajustes para deploy con seenode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import viteConfig from './vite.config.js'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [vue()],
+    vite: viteConfig
 });
