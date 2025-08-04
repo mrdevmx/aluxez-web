6754d29f93ee1c318617eb0f9a9b4d3d2c986d18
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-04 13:31:36 -0500

    fix: usar configuración TS de Vite para permitir hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import viteConfig from './vite.config.js'
+import viteConfig from './vite.config.ts'
 
 // https://astro.build/config
 export default defineConfig({
