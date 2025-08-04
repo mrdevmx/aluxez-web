33ad705e803eb4f00e6650dc5953907f7405b3a1
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-04 11:01:26 -0500

    Diseño de seccion soluciones

---
+++ src/components/Solutions.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mx-auto px-6 py-16 flex flex-wrap">
+        <div class="w-1/2">
+            <h2 class="text-5xl font-bold text-secondary leading-tight">
+                Creamos comunidades seguras, organizadas y armoniosas
+            </h2>
+            <p class="text-lg text-secondary-light mt-4">
+                Nuestro compromiso con la transparencia, la eficiencia y la protección nos convierte en el aliado confiable de cada residencial en Quintana Roo.
+            </p>
+        </div>
+        <div class="w-1/2">
+            <h2 class="text-5xl font-bold text-secondary leading-tight">
+                Creamos comunidades seguras, organizadas y armoniosas
+            </h2>
+            <p class="text-lg text-secondary-light mt-4">
+                Nuestro compromiso con la transparencia, la eficiencia y la protección nos convierte en el aliado confiable de cada residencial en Quintana Roo.
+            </p>
+        </div>
+    </div>
+</template>
