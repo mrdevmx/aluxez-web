52b0158ebad9068462999aba4a336206fcf1d893
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-04 14:06:00 -0500

    fix: agregando configuracion de vite en astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import viteConfig from './vite.config.js'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [vue()],
-    vite: viteConfig
+    server: {
+        host: '0.0.0.0',
+        port: 80
+    },
+    vite: {
+        preview: {
+            allowedHosts: [
+                'web-tx3htnl2yqa0.up-de-fra1-k8s-1.apps.run-on-seenode.com',
+                '.apps.run-on-seenode.com',
+                '*'
+            ],
+            host: '0.0.0.0',
+            port: 80,
+            strictPort: true
+        },
+        server: {
+            host: '0.0.0.0',
+            port: 80
+        }
+    }
 });
