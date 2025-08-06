204ad0439403d1af7491138a5854c7e4ff5acf44
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-06 13:30:02 -0500

    Agregando configuraciones extras

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 3000,
   },
   preview: {
-    host: true,
+    host: '0.0.0.0',
     port: 3000,
     allowedHosts: [
       'all',
