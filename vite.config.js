5d09a267c64183414bd16eef3af400d491fe82d9
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-06 13:05:54 -0500

    Agregando configuraciones extras

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     allowedHosts: [
+      'all',
       'localhost',
       '127.0.0.1',
-      '*'
+      '*',
+      'w8scwoos8kwg0s8cokggowco.154.122.229.178.sslip.io'
     ]
   }
 });
