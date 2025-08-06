58a0973321ef781d15d742cb2a5223bbdc583b1d
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-06 14:35:03 -0500

    Add correct allowed host for Coolify deployment

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     port: 3000,
     allowedHosts: [
       'all',
+      'xowww4osssw4oko4ggwo0k0k.154.12.229.178.sslip.io',
       'w8scwoos8kwg0s8cokggowco.154.122.229.178.sslip.io',
       'web-tx3htnl2yqa0.up-de-fra1-k8s-1.apps.run-on-seenode.com',
       '*'
