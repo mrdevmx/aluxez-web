2f96d4a66f22980193afb90c0b48a154289e2bbc
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-08-06 12:18:35 -0500

    Ajustando configuracion para coolify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,5 @@ import viteConfig from './vite.config.js';
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 3000
-    },
     vite: viteConfig
 });
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  preview: {
-    allowedHosts: [
-      'web-tx3htnl2yqa0.up-de-fra1-k8s-1.apps.run-on-seenode.com',
-      '.apps.run-on-seenode.com',
-      '.seenode.com',
-      'localhost',
-      '127.0.0.1',
-      '*'
-    ],
-    host: '0.0.0.0',
-    port: 80,
-    strictPort: false
-  },
   server: {
+    host: '0.0.0.0',
+    port: 3000,
     allowedHosts: [
-      'web-tx3htnl2yqa0.up-de-fra1-k8s-1.apps.run-on-seenode.com',
-      '.apps.run-on-seenode.com',
-      '.seenode.com',
       'localhost',
       '127.0.0.1',
       '*'
-    ],
-    host: '0.0.0.0',
-    port: 3000
+    ]
   }
 });
